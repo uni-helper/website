741174a95fa4ea0ef46666ae02060d4be3ec761e
kejun <kejun1997@gmail.com>
2023-04-12 23:28:53 +0800

    feat: add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: ["@unocss/nuxt", "@nuxtjs/color-mode"],
   css: ["@unocss/reset/tailwind.css"],
   colorMode: {
