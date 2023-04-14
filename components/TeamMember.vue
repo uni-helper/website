be351e13a61375887b5f383913161eab596c1f59
Neil <zguolee@foxmail.com>
2023-04-14 21:49:39 +0800

    chore: typo

--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -43,7 +43,10 @@ async function getExtraInfo(username: string) {
   <div grid="~ sm:cols-2 md:cols-3 lg:cols-4 gap-x-3 gap-y-20 items-center" p-10>
     <template v-for="member, _idx of members" :key="_idx">
       <div text-center>
-        <img loading="lazy" width="100" height="100" m-auto h-25 min-h-25 min-w-25 w-25 rounded-full :src="member.avatar">
+        <img
+          loading="lazy" width="100" height="100" m-auto h-25 min-h-25 min-w-25 w-25 rounded-full :src="member.avatar"
+          :alt="member.name"
+        >
         <div mb-1 mt-2 text-xl>
           {{ member.name }}
         </div>
@@ -53,12 +56,12 @@ async function getExtraInfo(username: string) {
         <div flex="~ inline gap-2" py-2>
           <a
             class="i-carbon-logo-github mya inline-block text-current op30 transition duration-200 hover:text-primary hover:op100"
-            :href="member.htmlUrl" target="_blank" rel="noopener noreferrer" aria-label="Anthony Fu on GitHub"
+            :href="member.htmlUrl" target="_blank" rel="noopener noreferrer" :aria-label="`${member.name} on GitHub`"
           />
           <a
             class="i-carbon-logo-twitter mya inline-block text-current op30 transition duration-200 hover:text-primary hover:op100"
             :href="`https://twitter.com/${member.twitter}`" target="_blank" rel="noopener noreferrer"
-            aria-label="Anthony Fu on Twitter"
+            :aria-label="`${member.name} on Twitter`"
           />
         </div>
       </div>
