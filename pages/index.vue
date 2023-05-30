723d6b61d53b5bbf61250cab70884cec7f365c64
Neil Lee <zguolee@foxmail.com>
2023-05-30 23:05:29 +0800

    styles: fix search input style

--- pages/index.vue
+++ pages/index.vue
@@ -25,13 +25,13 @@ const repoList = computed(() => repos.value?.filter((repo: any) => repo.name.toL
 <template>
   <div>
     <div
-      class="mb-4 flex items-center gap-2 rounded-md pl-4 text-xl" border="~ gray-200 dark:gray-700/60"
-      hover="border-primary dark:border-primary/60 transition-all"
+      class="mb-4 flex items-center gap-2 overflow-hidden rounded-md pl-4 text-xl" border="~ gray-200 dark:gray-700/60"
+      hover="border-primary dark:border-primary/60 transition-all" dark="bg-dark"
     >
       <div class="i-carbon-search op-60" />
       <input
-        v-model="searchVal" type="text" placeholder="快速搜索～" dark="bg-dark" w-full bg-light-100 py-2
-        outline="none active:none"
+        v-model="searchVal" type="text" placeholder="快速搜索～"
+        outline="none active:none" w-full bg-light-100 bg-transparent py-2
       >
     </div>
     <div grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4>
