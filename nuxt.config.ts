cfc9063fed83ae021f485b6116a59d9d2b0cd1dd
FliPPeDround <734243792@qq.com>
2025-08-19 16:08:19 +0800

    refactor(nuxt): convert nuxt config to async function

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,51 @@
 import { generatePrerenderRoutes } from './scripts/generate-routes'
 
-const prerenderRoutes = await generatePrerenderRoutes('./content')
+export default async () => {
+  const prerenderRoutes = await generatePrerenderRoutes('./content')
 
-export default defineNuxtConfig({
-  extends: ['shadcn-docs-nuxt'],
-  modules: [
-    'motion-v/nuxt',
-    '@nuxt/eslint',
-    '@unocss/nuxt',
-  ],
-  devtools: { enabled: true },
-  css: [
-    '@unocss/reset/tailwind.css',
-  ],
-  site: {
-    url: 'https://uni-helper.js.org/',
-  },
-  mdc: {
-    highlight: {
-      langs: ['jsonc'],
-    },
-  },
-  compatibilityDate: '2024-07-06',
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: prerenderRoutes,
-    },
-  },
-  eslint: {
-    config: {
-      standalone: false,
-      nuxt: {
-        sortConfigKeys: true,
-      },
-    },
-  },
-  i18n: {
-    defaultLocale: 'zh',
-    locales: [
-      {
-        code: 'zh',
-        name: '简体中文',
-        language: 'zh-CN',
-      },
+  return defineNuxtConfig({
+    extends: ['shadcn-docs-nuxt'],
+    modules: [
+      'motion-v/nuxt',
+      '@nuxt/eslint',
+      '@unocss/nuxt',
     ],
-  },
-})
+    devtools: { enabled: true },
+    css: [
+      '@unocss/reset/tailwind.css',
+    ],
+    site: {
+      url: 'https://uni-helper.js.org/',
+    },
+    mdc: {
+      highlight: {
+        langs: ['jsonc'],
+      },
+    },
+    compatibilityDate: '2024-07-06',
+    nitro: {
+      prerender: {
+        crawlLinks: true,
+        routes: prerenderRoutes,
+      },
+    },
+    eslint: {
+      config: {
+        standalone: false,
+        nuxt: {
+          sortConfigKeys: true,
+        },
+      },
+    },
+    i18n: {
+      defaultLocale: 'zh',
+      locales: [
+        {
+          code: 'zh',
+          name: '简体中文',
+          language: 'zh-CN',
+        },
+      ],
+    },
+  })
+}
