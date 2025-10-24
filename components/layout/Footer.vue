907a334ffd23c6d742a8ff83284db3d0cc26cd63
Panpan <wurui.dev@gmail.com>
2025-10-24 17:22:46 +0800

    feat: add police image and update footer for CN compliance

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const { footer } = useConfig().value
+
+const isCN = computed(() => {
+  if (typeof window === 'undefined')
+    return false
+
+  const hostname = window.location.hostname
+  return hostname === 'uni-helper.cn' || hostname.endsWith('.uni-helper.cn')
+})
+</script>
+
 <template>
   <footer class="text-muted-foreground py-6 md:px-8 md:py-0">
     <div class="container flex flex-col items-center justify-between gap-2 md:h-16 md:flex-row">
@@ -18,9 +30,12 @@
       </div>
     </div>
     <template v-if="isCN">
-      <div class="md:h-16 text-center leading-16 text-sm">
+      <div class="md:h-16 leading-16 text-sm flex justify-center items-center gap-x-6 flex-wrap [&_a]:font-semibold [&_a]:underline [&_a]:underline-offset-4">
+        <div class="flex items-center gap-2">
+          <img src="./police.png" width="20">
+          <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44060402003023" rel="noreferrer" target="_blank">粤公网安备44060402003023号</a>
+        </div>
         <a
-          class="font-semibold underline underline-offset-4"
           href="https://beian.miit.gov.cn/"
           target="_blank"
         >
@@ -30,14 +45,3 @@
     </template>
   </footer>
 </template>
-
-<script setup lang="ts">
-const { footer } = useConfig().value;
-
-const isCN = computed(() => {
-  if (typeof window === 'undefined') return false
-
-  const hostname = window.location.hostname
-  return hostname === 'uni-helper.cn' || hostname.endsWith('.uni-helper.cn')
-})
-</script>
